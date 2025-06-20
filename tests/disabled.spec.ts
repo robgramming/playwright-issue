5de756970a78927c291f4505881a581f27d26e4a
Rob Swindells <rob@borotec.co.uk>
2025-06-20 10:52:56 +0100

    Initial commit

---
+++ tests/disabled.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Playwright selects disabled options.', async ({ page }) => {
+  await page.goto("/");
+  
+  /* Select Option within a disabled optgroup:
+    Playwright selects the disabled option. (expected "element is not enabled" error") */
+  const select = page.locator("select#selectOptgroup");
+  await select.selectOption('carrot');
+  
+  /* Select a disabled option:
+    Playwright selects the disabled option. (expected "element is not enabled" error") */
+  const select1 = page.locator("select#selectDisabled");
+  await select1.selectOption('medium');
+  
+  /* Select an option in a disabled select:
+    this generates an "element is not enabled" error (which is expected) */
+  //const select2 = page.locator("select#disabledSelect");
+  //await select2.selectOption('optionB');
+});
